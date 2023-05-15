330a3447611026a6075dac30e3828e1e3a8362e3
Nick Howell <nrhowell@gmail.com>
2023-05-15 15:14:37 +1000

    Update Gaf_ODs_ImageJ.py

--- Gaf_ODs_ImageJ.py
+++ Gaf_ODs_ImageJ.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python
 
-#For caluclating optical density (OD), or absorbance, from Gafchromic film scanned with the Epson scanner and processed with imageJ
-#Takes the csv output from ImageJ ROI's and returns a second csv file containing %transmitance  and OD values
-#OD is calculated using the relationship 'OD = 2-log(%T)'
+"""For caluclating optical density (OD), or absorbance, from Gafchromic film scanned with the Epson scanner and processed with imageJ. 
+Two methods are given, one (OD) takes a series of ROI values, in csv format, and returns the corresponding OD's. The other (optical_density) takes an image file and transforms the image to return a image file calibrated to optical density
+
+OD is calculated using the relationship 'OD = 2-log(%T)'"""
 
 
 import pandas as pd
 import numpy as n
+import imageio
 from datetime import date
-
-x = str(input("ImageJ ROI file = "))
-y = float(input("Max transmission value = "))
 
 def OD(file,max_trans):
      
@@ -20,7 +19,17 @@ def OD(file,max_trans):
     data.to_csv(f'{file}_OD_{date.today()}.csv')
     #return(data)
 
-OD(x,y)
+
+def optical_density(input_path, output_path, maximum_transmission):
+    import imageio
+    import numpy as np
+
+    tiff_array = imageio.imread(input_path) #read the image file
+    trans_array = (tiff_array/maximum_transmission)*100 #calculate % trasmission
+    OD_array = 2-n.log10(trans_array)  #calculate the optical densities
+    imageio.imwrite(output_path, OD_array) #write the array to a new image file
+    print("TIFF image file saved to", output_path)
+
 
 
 #Author: Nick Howell (nrhowell@gmail.com)
